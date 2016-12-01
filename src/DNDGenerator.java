ca985b8140b1abf0c3e1b58303e61c04781f2d52
Nikolay Troshkov <n.trshkv@gmail.com>
2016-12-01 06:06:43 +0300

    update main class

--- src/DNDGenerator.java
+++ src/DNDGenerator.java
@@ -1,4 +1,4 @@
-import MapGenerator.MC;
+import MapGenerator.MapMaker;
 import storygen.StoryGenerator;
 import storygen.entity.LocationType;
 
@@ -15,13 +15,18 @@ import java.util.Map;
 public class DNDGenerator {
     public static void main(String[] args) {
         try {
-            new MC();
+            long time = System.currentTimeMillis();
+            String foldName = "Results/" + time;
+            File d = new File(foldName);
+            d.mkdir();
+
+            new MapMaker(time);
 
             Map<LocationType, Integer> locations = new EnumMap<>(LocationType.class);
             List<Integer> locationsCountUtil = new ArrayList<>(1);
             locationsCountUtil.add(0);
             try {
-                Files.lines(Paths.get("locations.dnd")).forEach(s -> {
+                Files.lines(Paths.get(foldName + "locations.dnd")).forEach(s -> {
                     String[] parts = s.split(":");
                     locationsCountUtil.set(0, locationsCountUtil.get(0) + Integer.valueOf(parts[1]));
                     locations.put(LocationType.valueOf(parts[0].toUpperCase()), Integer.valueOf(parts[1]));
@@ -38,9 +43,6 @@ public class DNDGenerator {
                 stories.append(generator.generate()).append("\n\n");
             }
 
-            String foldName = "Results/" + System.currentTimeMillis();
-            File d = new File(foldName);
-            d.mkdir();
             PrintWriter writer = new PrintWriter(foldName + "/quests_base.txt", "UTF-8");
             writer.println(stories.toString());
             writer.close();
